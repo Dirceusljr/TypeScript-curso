271916b2935f2fb232b77610156d4afcd54ddb63
Dirceusljr <dirjunior18@gmail.com>
2024-03-25 14:04:11 -0300

    projeto curso 2 completo

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,18 +1,4 @@
 export class Negociacao {
-    // constructor(
-    //     private _data: Date,
-    //     private _quantidade: number,
-    //     private _valor: number,
-    // ) {}
-    // get data(): Date {
-    //     return this._data
-    // }
-    // get quantidade(): number {
-    //     return this._quantidade
-    // }
-    // get valor(): number {
-    //     return this._valor
-    // }
     constructor(_data, quantidade, valor) {
         this._data = _data;
         this.quantidade = quantidade;
